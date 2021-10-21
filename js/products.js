14ef3ed458fc18d52b91ac3ee76390b29b0a33ce
unknown <twolfdota@gmail.com>
2021-10-21 16:27:10 +0700

    finished project

--- js/products.js
+++ js/products.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    getProducts("?cat=Food&_limit=4",".food-list");
-    getProducts("?cat=Veg&_limit=4", ".veg-list");
-    getProducts("?cat=Bevs&_limit=4", ".bevs-list");
+    getProducts("?cat=Food&_limit=4",".food-list", false);
+    getProducts("?cat=Veg&_limit=4", ".veg-list", false);
+    getProducts("?cat=Bevs&_limit=4", ".bevs-list", false);
 })
